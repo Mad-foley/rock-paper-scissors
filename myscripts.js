5062d725c01132798dd5c8be32aa56e7111af146
Maddy Foley <maddyfoley5@gmail.com>
2022-09-27 09:42:53 -0400

    added winner function and logs winner to console

--- myscripts.js
+++ myscripts.js
@@ -6,4 +6,18 @@ function getComputerChoice() {
 }
 
 let computerChoice = getComputerChoice();
-//console.log(computerChoice(playerChoice, computerChoice))
+
+function winner (player, computer) {
+  if(choices.includes(player)) {
+    if(player === computer) {
+      return "Tie"
+    } else if ((player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper') || (player === 'rock' && computer === 'scissors')) {
+      return "You win!"
+    } else {
+      return "Computer wins!"
+    }
+  }
+  return "Please input: rock, paper or scissors"
+}
+
+console.log(winner(playerChoice, computerChoice))
